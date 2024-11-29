ba27b55b45ce932152600cd732238f48a8797e1d
Kathy Zhu <zhuk50@mcmaster.ca>
2024-11-29 00:11:54 -0500

    Part 1A Done

--- Player.cpp
+++ Player.cpp
@@ -6,28 +6,80 @@ Player::Player(GameMechs* thisGMRef)
     mainGameMechsRef = thisGMRef;
     myDir = STOP;
 
-    // more actions to be included
+    int board_x = mainGameMechsRef->getBoardSizeX();
+    int board_y = mainGameMechsRef->getBoardSizeY();
+
+    playerPos.setObjPos(board_x*0.5, board_y*0.5, '*');
+
 }
 
 
 Player::~Player()
 {
-    // delete any heap members here
+    // :3
 }
 
 objPos Player::getPlayerPos() const
 {
-    // return the reference to the playerPos arrray list
+    return playerPos.getObjPos();
 }
 
 void Player::updatePlayerDir()
 {
-        // PPA3 input processing logic          
+    char input = mainGameMechsRef->getInput();
+    switch(input) { 
+        case 'w':
+            if(myDir != DOWN) myDir = UP;
+            break;
+        case 'a':
+            if(myDir != RIGHT) myDir = LEFT;
+            break;
+        case 's':
+            if(myDir != UP) myDir = DOWN;
+            break;
+        case 'd':
+            if(myDir != LEFT) myDir = RIGHT;
+            break;
+        default:
+            break;
+    }         
 }
 
 void Player::movePlayer()
 {
-    // PPA3 Finite State Machine logic
+    int board_x = mainGameMechsRef->getBoardSizeX();
+    int board_y = mainGameMechsRef->getBoardSizeY();
+
+    int x = playerPos.pos->x;
+    int y = playerPos.pos->y;
+    switch(myDir) {
+        case LEFT:
+            x--;
+            break;
+        case RIGHT:
+            x++;
+            break;
+        case UP:
+            y--;
+            break;
+        case DOWN:
+            y++;
+            break;
+        case STOP:
+        default:
+            break;
+    }
+    if(x >= board_x) {
+        x = 1;
+    } else if(x <= 0) {
+        x = board_x -1;
+    }
+    if(y >= board_y) {
+        y = 1;
+    } else if(y <= 0) {
+        y = board_y -1;
+    }
+    
 }
 
 // More methods to be added
--- Project.cpp
+++ Project.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include "MacUILib.h"
 #include "objPos.h"
+#include "GameMechs.h"
+#include "Player.h"
 
 using namespace std;
 
 #define DELAY_CONST 100000
 
-bool exitFlag;
+bool exitFlag; // delete this
+Player* snake;
+GameMechs* gamemechs;
 
 void Initialize(void);
 void GetInput(void);
@@ -40,6 +44,8 @@ void Initialize(void)
     MacUILib_init();
     MacUILib_clearScreen();
 
+    gamemechs = new GameMechs();
+    snake = new Player(gamemechs);
     exitFlag = false;
 }
 
@@ -55,7 +61,24 @@ void RunLogic(void)
 
 void DrawScreen(void)
 {
-    MacUILib_clearScreen();    
+    MacUILib_clearScreen();   
+    int board_x = gamemechs->getBoardSizeX();
+    int board_y = gamemechs->getBoardSizeY();
+    int player_x = snake->getPlayerPos().pos->x;
+    int player_y = snake->getPlayerPos().pos->y;
+    for(int y = 0; y < board_y; y++) {
+        for(int x = 0; x < board_x; x++) {
+            if(x == 0 || x == board_x - 1 || y == 0 || y == board_y - 1) {
+                MacUILib_printf("#");
+            } else if(x == player_x && y == player_y) {
+                MacUILib_printf("*");
+            } else {
+                MacUILib_printf(" ");
+            }
+            
+        }
+        MacUILib_printf("\n");
+    }
 }
 
 void LoopDelay(void)
@@ -66,7 +89,10 @@ void LoopDelay(void)
 
 void CleanUp(void)
 {
-    MacUILib_clearScreen();    
+    MacUILib_clearScreen();  
+    delete gamemechs;
+    delete snake;  
 
     MacUILib_uninit();
+
 }
